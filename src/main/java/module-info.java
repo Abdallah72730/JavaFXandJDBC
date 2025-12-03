8d38c734e63c893de944ac6fe870b06850b56ef8
Abdallah72730 <ianomeit@gmail.com>
2025-12-02 19:44:40 -0700

    creating JavaFX with DB connection project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.example.javafxandjdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.javafxandjdbc to javafx.fxml;
+    exports org.example.javafxandjdbc;
+}
